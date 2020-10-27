4a901369bbb2691962884a971830f262e30a9f55
Kwansy98 <40238421+Kwansy98@users.noreply.github.com>
2020-10-27 12:17:06 +0800

    [001] 2020-10-27 11:28:04
    第一次提交

---
+++ MsgBoxAWatcher_Ring0/MsgBoxAWatcher_Ring0.c
@@ -0,0 +1,486 @@
+// C/C++ -> 代码生成
+// 基本运行时检查：默认值
+// 缓冲区安全检查：否
+
+#include <ntifs.h>
+#include <wdm.h>
+
+#define DEVICE_NAME L"\\Device\\MsgBoxAWatcherDriverDev"
+#define DRIVER_LINK L"\\??\\MsgBoxAWatcherDriverLnk"
+
+// 申请了4KB设备扩展内存，用于替代全局变量
+// 0-3字节：调用门描述符地址（GDT）
+// 4-7字节：中断门描述符地址（IDT）
+#define DeviceExtendSize 0x1000
+
+// 3环发 IRP_MJ_DEVICE_CONTROL 的操作编号
+#define OPER_CALL_GATE_R0 CTL_CODE(FILE_DEVICE_UNKNOWN,0x800,METHOD_BUFFERED,FILE_ANY_ACCESS)
+#define OPER_HOOK CTL_CODE(FILE_DEVICE_UNKNOWN,0x801,METHOD_BUFFERED,FILE_ANY_ACCESS)
+#define OPER_GET_APICALLRECORD CTL_CODE(FILE_DEVICE_UNKNOWN,0x802,METHOD_BUFFERED,FILE_ANY_ACCESS)
+
+
+// 结构声明
+typedef struct _LDR_DATA_TABLE_ENTRY
+{
+	LIST_ENTRY InLoadOrderLinks;
+	LIST_ENTRY InMemoryOrderLinks;
+	LIST_ENTRY InInitializationOrderLinks;
+	PVOID DllBase;
+	PVOID EntryPoint;
+	UINT32 SizeOfImage;
+	UNICODE_STRING FullDllName;
+	UNICODE_STRING BaseDllName;
+	UINT32 Flags;
+	USHORT LoadCount;
+	USHORT TlsIndex;
+	LIST_ENTRY HashLinks;
+	PVOID SectionPointer;
+	UINT32 CheckSum;
+	UINT32 TimeDateStamp;
+	PVOID LoadedImports;
+	PVOID EntryPointActivationContext;
+	PVOID PatchInformation;
+} LDR_DATA_TABLE_ENTRY, *PLDR_DATA_TABLE_ENTRY;
+
+// API调用记录父类
+typedef struct _APICALLRECORD
+{
+	LIST_ENTRY ApiCallRecordList; // 链表
+	UINT32 pApiAddress; // API函数地址
+	UINT32 nParam; // 参数个数
+	UINT32 Param[32]; // 参数列表
+} APICALLRECORD, *PAPICALLRECORD;
+
+// 全局变量
+PDEVICE_OBJECT g_pDevObj = NULL; // 自定义设备，用于和3环通信
+APICALLRECORD g_ApiCallRecordQueue = { 0 }; // API调用记录队列，不要直接操作该链表，使用程序提供的API
+
+// 函数声明
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriver, PUNICODE_STRING RegPath);
+VOID DriverUnload(PDRIVER_OBJECT pDriver);
+NTSTATUS IrpCreateProc(PDEVICE_OBJECT pDevObj, PIRP pIrp);
+NTSTATUS IrpCloseProc(PDEVICE_OBJECT pDevObj, PIRP pIrp);
+NTSTATUS IrpDeviceControlProc(PDEVICE_OBJECT pDevObj, PIRP pIrp);
+VOID GetKernelBase(PDRIVER_OBJECT driver, PVOID *pKrnlBase, PUINT32 uKrnlImageSize);
+PVOID MemorySearch(PVOID bytecode, UINT32 bytecodeLen, PVOID pBeginAddress, PVOID pEndAddress);
+UINT32 *GetPDE(UINT32 addr);
+UINT32 *GetPTE(UINT32 addr);
+USHORT SetCallGate(UINT32 pFunction, UINT32 nParam);
+USHORT SetIntGate(UINT32 pFuncion);
+void User32ApiSpyNaked();
+void __stdcall User32ApiSpy(UINT32 ESP3, UINT32 EIP3);
+void InitApiCallQueue(IN PAPICALLRECORD QueueHead);
+void PushApiCallQueue(IN PAPICALLRECORD QueueHead, IN PAPICALLRECORD pApiCallRecord);
+void PopApiCallQueue(IN PAPICALLRECORD QueueHead, OUT PAPICALLRECORD * pApiCallRecord);
+UINT32 GetCountApiCallQueue(IN PAPICALLRECORD QueueHead);
+void FreeApiCallQueue(IN PAPICALLRECORD QueueHead);
+
+// 入口函数
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriver, PUNICODE_STRING RegPath){
+	NTSTATUS status;
+	ULONG uIndex = 0;
+	PDEVICE_OBJECT pDeviceObj = NULL; // 设备对象指针
+	UNICODE_STRING DeviceName; // 设备名，0环用
+	UNICODE_STRING SymbolicLinkName; // 符号链接名，3环用
+
+	// 初始化调用记录队列
+	InitApiCallQueue(&g_ApiCallRecordQueue);
+
+	// 创建设备名称
+	RtlInitUnicodeString(&DeviceName,DEVICE_NAME);
+	// 创建设备	
+	status = IoCreateDevice(pDriver,DeviceExtendSize,&DeviceName,FILE_DEVICE_UNKNOWN,FILE_DEVICE_SECURE_OPEN,FALSE,&pDeviceObj);
+	if (status != STATUS_SUCCESS)
+	{
+		IoDeleteDevice(pDeviceObj);
+		DbgPrint("创建设备失败.\n");
+		return status;
+	}
+	// 全局变量依赖于设备扩展内存
+	// 初始化全局设备指针
+	g_pDevObj = pDeviceObj;	
+	// 初始化设备扩展数据
+	memset(pDeviceObj->DeviceExtension,0,DeviceExtendSize);
+	//DbgPrint("创建设备成功.\n");
+	// 设置交互数据的方式
+	pDeviceObj->Flags |= DO_BUFFERED_IO;
+	// 创建符号链接
+	RtlInitUnicodeString(&SymbolicLinkName, DRIVER_LINK);
+	IoCreateSymbolicLink(&SymbolicLinkName, &DeviceName);
+	// 设置分发函数
+	pDriver->MajorFunction[IRP_MJ_CREATE] = IrpCreateProc;
+	pDriver->MajorFunction[IRP_MJ_CLOSE] = IrpCloseProc;
+	pDriver->MajorFunction[IRP_MJ_DEVICE_CONTROL] = IrpDeviceControlProc;
+
+	// 设置卸载函数
+	pDriver->DriverUnload = DriverUnload;
+	return STATUS_SUCCESS;
+}
+
+// 卸载驱动
+VOID DriverUnload(PDRIVER_OBJECT pDriver)
+{
+	UNICODE_STRING SymbolicLinkName;
+	// 删除GDT表项中的调用门
+	memset((PVOID)((PUINT32)(pDriver->DeviceObject->DeviceExtension))[0],0,8);
+	// 删除IDT表中的中断门
+	memset((PVOID)(((PUINT32)(pDriver->DeviceObject->DeviceExtension))[1]),0,8);
+	// 释放队列内存
+	//DbgPrint("队列长度：%d\n", GetCountApiCallQueue(&g_ApiCallRecordQueue));
+	FreeApiCallQueue(&g_ApiCallRecordQueue);
+	//DbgPrint("队列长度：%d\n", GetCountApiCallQueue(&g_ApiCallRecordQueue));
+	// 删除符号链接，删除设备
+	RtlInitUnicodeString(&SymbolicLinkName, DRIVER_LINK);
+	IoDeleteSymbolicLink(&SymbolicLinkName);
+	IoDeleteDevice(pDriver->DeviceObject);
+	DbgPrint("驱动卸载成功\n");
+}
+
+// 不设置这个函数，则Ring3调用CreateFile会返回1
+// IRP_MJ_CREATE 处理函数
+NTSTATUS IrpCreateProc(PDEVICE_OBJECT pDevObj, PIRP pIrp)
+{
+	//DbgPrint("应用层连接设备.\n");
+	// 返回状态如果不设置，Ring3返回值是失败
+	pIrp->IoStatus.Status = STATUS_SUCCESS;
+	pIrp->IoStatus.Information = 0;
+	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+}
+
+// IRP_MJ_CLOSE 处理函数
+NTSTATUS IrpCloseProc(PDEVICE_OBJECT pDevObj, PIRP pIrp)
+{
+	//DbgPrint("应用层断开连接设备.\n");
+	// 返回状态如果不设置，Ring3返回值是失败
+	pIrp->IoStatus.Status = STATUS_SUCCESS;
+	pIrp->IoStatus.Information = 0;
+	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+}
+
+// IRP_MJ_DEVICE_CONTROL 处理函数
+NTSTATUS IrpDeviceControlProc(PDEVICE_OBJECT pDevObj, PIRP pIrp)
+{
+	NTSTATUS status = STATUS_INVALID_DEVICE_REQUEST;
+	PIO_STACK_LOCATION pIrpStack;
+	ULONG uIoControlCode;
+	PVOID pIoBuffer;
+	ULONG uInLength;
+	ULONG uOutLength;
+
+	// 获取IRP数据
+	pIrpStack = IoGetCurrentIrpStackLocation(pIrp);
+	// 获取控制码
+	uIoControlCode = pIrpStack->Parameters.DeviceIoControl.IoControlCode;
+	// 获取缓冲区地址（输入输出是同一个）
+	pIoBuffer = pIrp->AssociatedIrp.SystemBuffer;
+	// Ring3 发送数据的长度
+	uInLength = pIrpStack->Parameters.DeviceIoControl.InputBufferLength;
+	// Ring0 发送数据的长度
+	uOutLength = pIrpStack->Parameters.DeviceIoControl.OutputBufferLength;
+
+	switch (uIoControlCode)
+	{
+	case OPER_CALL_GATE_R0:
+		{
+			UINT32 pFunction; // 3环函数指针
+			UINT32 nParam; // 参数个数
+			// 给3环传进来的函数指针设置一个调用门
+			pFunction = ((PUINT32)pIoBuffer)[0];
+			nParam = ((PUINT32)pIoBuffer)[1];
+			// 设置状态，返回数据
+			((PUSHORT)pIoBuffer)[0] = SetCallGate(pFunction, nParam); // 返回调用门选择子
+			pIrp->IoStatus.Information = 2; // 返回给3环的数据量
+			status = STATUS_SUCCESS;
+			break;
+		}
+	case OPER_HOOK:
+		{
+			// 返回给3环的中断号，3环根据中断号HOOK API
+			USHORT IntGateNum;
+			// 构造提权中断门
+			IntGateNum = SetIntGate((UINT32)User32ApiSpyNaked);
+			// 返回中断号
+			*(PUSHORT)pIoBuffer = IntGateNum;
+			// 设置状态，返回数据
+			pIrp->IoStatus.Information = 2; // 返回给3环的数据量
+			status = STATUS_SUCCESS;
+			break;
+		}
+	case OPER_GET_APICALLRECORD:
+		{
+			PAPICALLRECORD record = NULL;
+			PopApiCallQueue(&g_ApiCallRecordQueue, &record);
+			if (record == NULL)
+			{
+				// 设置状态，返回数据
+				pIrp->IoStatus.Information = 0; // 返回给3环的数据量
+				status = STATUS_SUCCESS;
+			}
+			else
+			{
+				memcpy(pIoBuffer, record, sizeof(APICALLRECORD));
+				// 设置状态，返回数据
+				pIrp->IoStatus.Information = sizeof(APICALLRECORD); // 返回给3环的数据量
+				status = STATUS_SUCCESS;
+			}
+			break;
+		}
+	}
+
+	// 返回状态如果不设置，Ring3返回值是失败
+	pIrp->IoStatus.Status = status;
+	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+}
+
+// 获取内核基址，大小
+VOID GetKernelBase(PDRIVER_OBJECT driver, PVOID *pKrnlBase, PUINT32 uKrnlImageSize)
+{
+	PLDR_DATA_TABLE_ENTRY pLdteHead; // 内核模块链表头
+	PLDR_DATA_TABLE_ENTRY pLdteCur; // 遍历指针
+	UNICODE_STRING usKrnlBaseDllName; // 内核模块名
+
+	RtlInitUnicodeString(&usKrnlBaseDllName,L"ntoskrnl.exe");
+	pLdteHead = (PLDR_DATA_TABLE_ENTRY)driver->DriverSection;
+	pLdteCur = pLdteHead;
+	do 
+	{
+		PLDR_DATA_TABLE_ENTRY pLdte = CONTAINING_RECORD(pLdteCur, LDR_DATA_TABLE_ENTRY, InLoadOrderLinks);
+		//DbgPrint("DllBase: %p, SizeOfImage: %08X %wZ\n", pLdteCur->DllBase, pLdteCur->SizeOfImage, &(pLdteCur->FullDllName));
+		if (RtlCompareUnicodeString(&pLdteCur->BaseDllName, &usKrnlBaseDllName, TRUE) == 0)
+		{
+			*pKrnlBase = pLdteCur->DllBase;
+			*uKrnlImageSize = pLdteCur->SizeOfImage;
+			return;
+		}
+		pLdteCur = (PLDR_DATA_TABLE_ENTRY)pLdteCur->InLoadOrderLinks.Flink;
+	} while (pLdteHead != pLdteCur);
+	return;
+}
+
+// 特征码搜索
+PVOID MemorySearch(PVOID bytecode, UINT32 bytecodeLen, PVOID pBeginAddress, PVOID pEndAddress)
+{
+	PVOID pCur = pBeginAddress;
+	while (pCur != pEndAddress)
+	{
+		if (RtlCompareMemory(bytecode,pCur,bytecodeLen) == bytecodeLen)
+		{
+			return pCur;
+		}
+		((UINT32)pCur)++;
+	}
+	return 0;
+}
+
+// 构造提权中断门，返回中断号
+USHORT SetIntGate(UINT32 pFuncion)
+{	
+	UCHAR IDT[6]; // IDT寄存器
+	UINT32 IdtAddr,IdtLen;
+	UINT32 IntGateHi = 0,IntGateLo = 0; // 中断门描述符
+	UINT32 *pPreIntGateAddr = (UINT32*)g_pDevObj->DeviceExtension + 1;
+	UINT32 i;
+	// 构造中断门描述符
+	IntGateLo = ((pFuncion & 0x0000FFFF) | 0x00080000);
+	IntGateHi = ((pFuncion & 0xFFFF0000) | 0x0000EE00);
+	// 遍历IDT，找无效项
+	__asm
+	{
+		sidt fword ptr IDT;
+	}
+	IdtAddr = *(PULONG)(IDT+2);
+	IdtLen = *(PUSHORT)IDT;
+	// 遍历IDT，找一个P=0的（跳过第一项）
+	if ((*pPreIntGateAddr) == 0)
+	{				
+		for (i = 8; i < IdtLen; i+=8)
+		{
+			if ((((PUINT32)(IdtAddr + i))[1] & 0x00008000) == 0)
+			{
+				// P=0，此处GDT表项无效，可以使用
+				((PUINT32)(IdtAddr + i))[0] = IntGateLo;
+				((PUINT32)(IdtAddr + i))[1] = IntGateHi;
+				(*pPreIntGateAddr) = IdtAddr + i;				
+				break;
+			}
+		}
+	}
+	else
+	{
+		((PUINT32)(*pPreIntGateAddr))[0] = IntGateLo;
+		((PUINT32)(*pPreIntGateAddr))[1] = IntGateHi;
+	}
+
+	//DbgPrint("*pPreIntGateAddr: %p.\n", *pPreIntGateAddr);
+	//DbgPrint("INT %02X\n", (USHORT)((*pPreIntGateAddr - IdtAddr) / 8));
+	if (*pPreIntGateAddr == 0) return 0;
+	return (USHORT)((*pPreIntGateAddr - IdtAddr) / 8);
+}
+
+// 构造提权调用门，返回调用门选择子
+USHORT SetCallGate(UINT32 pFunction, UINT32 nParam)
+{	
+	UINT32 CallGateHi = 0,CallGateLo = 0; // 调用门描述符
+	UCHAR GDT[6]; // GDT寄存器
+	UINT32 GdtAddr,GdtLen;
+	UINT32 i;
+	UINT32 *pPreCallGateAddr = (UINT32*)g_pDevObj->DeviceExtension;
+
+	// 构造调用门
+	CallGateHi = (pFunction & 0xFFFF0000);
+	CallGateHi |= 0x0000EC00;
+	CallGateHi |= nParam;
+	CallGateLo = (pFunction & 0x0000FFFF);
+	CallGateLo |= 0x00080000;
+	// 获取GDT基址和大小
+	__asm
+	{
+		sgdt fword ptr GDT;
+	}
+	GdtAddr = *(PULONG)(GDT+2);
+	GdtLen = *(PUSHORT)GDT;
+	// 遍历GDT，找一个P=0的（跳过第一项）
+	if ((*pPreCallGateAddr) == 0)
+	{				
+		for (i = 8; i < GdtLen; i+=8)
+		{
+			//DbgPrint("%p\n",(PUINT32)(GdtAddr + i));
+			if ((((PUINT32)(GdtAddr + i))[1] & 0x00008000) == 0)
+			{
+				// P=0，此处GDT表项无效，可以使用
+				((PUINT32)(GdtAddr + i))[0] = CallGateLo;
+				((PUINT32)(GdtAddr + i))[1] = CallGateHi;
+				(*pPreCallGateAddr) = GdtAddr + i;
+				break;
+			}
+		}
+	}
+	else
+	{
+		((PUINT32)(*pPreCallGateAddr))[0] = CallGateLo;
+		((PUINT32)(*pPreCallGateAddr))[1] = CallGateHi;
+	}
+	if (*pPreCallGateAddr == 0) return 0;
+	return (USHORT)((*pPreCallGateAddr) - GdtAddr);
+}
+
+// 获取PDE
+UINT32 *GetPDE(UINT32 addr)
+{
+	return (UINT32 *)(0xc0600000 + ((addr >> 18) & 0x3ff8));
+}
+
+// 获取PTE
+UINT32 *GetPTE(UINT32 addr)
+{
+	return (UINT32 *)(0xc0000000 + ((addr >> 9) & 0x7ffff8));
+}
+
+// User32.dll 导出函数的钩子函数
+// 调用方式：修改API函数头2字节，使API函数触发中断，通过提权中断门调用本函数
+void __declspec(naked) User32ApiSpyNaked()
+{
+	__asm
+	{
+		pushad; // esp - 0x20
+		pushfd; // esp - 0x04
+
+		mov eax,[esp + 0x24];		
+		mov ecx,[esp + 0x24 + 0x0C];
+		push eax; // EIP3
+		push ecx; // ESP3
+		call User32ApiSpy;
+
+		popfd;
+		popad;
+		iretd;
+	}
+}
+
+// 此处需要完成的工作：读取3环EIP，判断API来源，读取3环ESP，获取参数，传给3环控制程序
+void __stdcall User32ApiSpy(UINT32 ESP3, UINT32 EIP3)
+{
+	UINT32 ApiAddress;
+	// EIP3-0x02是API的地址
+	// ESP3是3环的ESP，可以用来读参数
+	__asm push fs;
+	ApiAddress = EIP3 - 2;
+	//DbgPrint("ESP3: %08x, API: %08x\n", ESP3, ApiAddress);
+	// 判断API地址
+	if (ApiAddress == 0x77d507ea)
+	{
+		PAPICALLRECORD pApiCallRecord = NULL;
+		// 添加调用记录到队列，监视进程通过IRP消息读取队列
+		pApiCallRecord = (PAPICALLRECORD)ExAllocatePool(PagedPool,sizeof(APICALLRECORD));
+		pApiCallRecord->nParam = 4;
+		pApiCallRecord->pApiAddress = ApiAddress;
+		pApiCallRecord->Param[0] = ((PUINT32)ESP3)[1];
+		pApiCallRecord->Param[1] = ((PUINT32)ESP3)[2];
+		pApiCallRecord->Param[2] = ((PUINT32)ESP3)[3];
+		pApiCallRecord->Param[3] = ((PUINT32)ESP3)[4];
+		PushApiCallQueue(&g_ApiCallRecordQueue, (PAPICALLRECORD)pApiCallRecord);
+	}
+	__asm pop fs;
+}
+
+// 初始化队列
+void InitApiCallQueue(IN PAPICALLRECORD QueueHead)
+{
+	QueueHead->ApiCallRecordList.Flink = QueueHead->ApiCallRecordList.Blink = (PLIST_ENTRY)QueueHead;
+}
+
+// 插入一条调用记录到队尾
+void PushApiCallQueue(IN PAPICALLRECORD QueueHead, IN PAPICALLRECORD pApiCallRecord)
+{
+	// 原队尾的下一个节点指向新队尾
+	QueueHead->ApiCallRecordList.Blink->Flink = (PLIST_ENTRY)pApiCallRecord;
+	// 新队尾的上一个节点指向原队尾
+	pApiCallRecord->ApiCallRecordList.Blink = QueueHead->ApiCallRecordList.Blink;
+	// 新队尾的下一个节点指向队列头
+	pApiCallRecord->ApiCallRecordList.Flink = (PLIST_ENTRY)QueueHead;
+	// 队列头的上一个节点指向新队尾
+	QueueHead->ApiCallRecordList.Blink = (PLIST_ENTRY)pApiCallRecord;	
+}
+
+// 从队首弹出一条调用记录
+void PopApiCallQueue(IN PAPICALLRECORD QueueHead, OUT PAPICALLRECORD * pApiCallRecord)
+{
+	// 记录要弹出的节点
+	*pApiCallRecord = (PAPICALLRECORD)(QueueHead->ApiCallRecordList.Flink);
+	// 如果队列为空，返回NULL
+	if (*pApiCallRecord == &g_ApiCallRecordQueue)
+	{
+		*pApiCallRecord = NULL;
+	}
+	// 第二个节点的上一个节点指向队首
+	QueueHead->ApiCallRecordList.Flink->Flink->Blink = (PLIST_ENTRY)QueueHead;
+	// 队首的下一个节点指向第二个节点
+	QueueHead->ApiCallRecordList.Flink = QueueHead->ApiCallRecordList.Flink->Flink;
+}
+
+// 计算队列长度
+UINT32 GetCountApiCallQueue(IN PAPICALLRECORD QueueHead)
+{
+	UINT32 cnt = 0;
+	PLIST_ENTRY pList = QueueHead->ApiCallRecordList.Flink;
+	while (pList != (PLIST_ENTRY)QueueHead)
+	{
+		pList = pList->Flink;
+		cnt++;
+	}
+	return cnt;
+}
+
+// 释放队列内存
+void FreeApiCallQueue(IN PAPICALLRECORD QueueHead)
+{
+	PAPICALLRECORD pApiCallRecord;
+	while(QueueHead->ApiCallRecordList.Flink != (PLIST_ENTRY)QueueHead)
+	{		
+		PopApiCallQueue(QueueHead, &pApiCallRecord);
+		ExFreePool(pApiCallRecord);
+	}
+}
---
+++ MsgBoxAWatcher_Ring3/MsgBoxAWatcher_Ring3.cpp
@@ -0,0 +1,341 @@
+// MsgBoxAWatcher_Ring3.cpp : 定义控制台应用程序的入口点。
+//
+//1、自己写代码加载，卸载驱动程序
+//2、段页的知识；绕写拷贝
+//3、写HOOK
+//4、shellcode
+
+// C/C++ -> 代码生成
+// 基本运行时检查：默认值
+// 缓冲区安全检查：否
+
+
+#include "stdafx.h"
+#include <Windows.h>
+
+#define DRIVER_NAME L"MsgBoxAWatcher_Ring0"
+#define DRIVER_PATH L"MsgBoxAWatcher_Ring0.sys"
+#define DRIVER_LINK L"\\\\.\\MsgBoxAWatcherDriverLnk"
+
+#define OPER_CALL_GATE_R0 CTL_CODE(FILE_DEVICE_UNKNOWN,0x800,METHOD_BUFFERED,FILE_ANY_ACCESS)
+#define OPER_HOOK CTL_CODE(FILE_DEVICE_UNKNOWN,0x801,METHOD_BUFFERED,FILE_ANY_ACCESS)
+#define OPER_GET_APICALLRECORD CTL_CODE(FILE_DEVICE_UNKNOWN,0x802,METHOD_BUFFERED,FILE_ANY_ACCESS)
+
+// API调用记录父类
+typedef struct _APICALLRECORD
+{
+	LIST_ENTRY ApiCallRecordList; // 链表
+	UINT32 pApiAddress; // API函数地址
+	UINT32 nParam; // 参数个数
+	UINT32 Param[32]; // 参数列表
+} APICALLRECORD, *PAPICALLRECORD;
+
+BOOL LoadDriver(PCWSTR lpszDriverName, PCWSTR lpszDriverPath);
+void UnLoadDriver(PCWSTR lpszDriverName);
+DWORD *GetPDE(DWORD addr);
+DWORD *GetPTE(DWORD addr);
+USHORT CreateCallGate(DWORD pBaseAddress, DWORD nParam);
+BOOL CallInRing0(PVOID pFuncion, PDWORD pParam, DWORD nParam);
+void BypassApiWriteCopyNaked();
+void BypassApiWriteCopy();
+BOOL HookUser32Api();
+void UpdateApiCallRecord();
+
+int _tmain(int argc, _TCHAR* argv[])
+{	
+	// 加载驱动
+	if (!LoadDriver(DRIVER_NAME, DRIVER_PATH))
+	{
+		printf("驱动服务加载失败.\n");
+		getchar();
+		return 1;
+	}
+	else
+	{
+		printf("驱动服务加载成功.\n");
+	}
+
+	// 过写拷贝
+	BypassApiWriteCopy();
+
+	// HOOK MessageBoxA
+	if (HookUser32Api())
+	{
+		printf("HOOK MessageBoxA 成功，现在可以在其他程序里调用 MessageBoxA.\n");		
+	}
+	else
+	{
+		printf("HOOK MessageBoxA 失败.\n");
+	}
+	// 读取调用记录
+	UpdateApiCallRecord();
+
+	// 取消HOOK
+	((PUSHORT)MessageBoxA)[0] = 0xff8b;
+	UnLoadDriver(DRIVER_NAME);
+	printf("敲任意按键退出程序.\n");
+	getchar();
+	return 0;
+}
+
+// 加载驱动
+BOOL LoadDriver(PCWSTR lpszDriverName, PCWSTR lpszDriverPath)
+{
+	// 获取驱动完整路径
+	WCHAR szDriverFullPath[MAX_PATH] = { 0 };
+	GetFullPathNameW(lpszDriverPath,MAX_PATH,szDriverFullPath,NULL);
+	//printf("%s\n", szDriverFullPath);
+	// 打开服务控制管理器
+	SC_HANDLE hServiceMgr = NULL; // SCM管理器句柄	
+	hServiceMgr = OpenSCManagerW(NULL,NULL,SC_MANAGER_ALL_ACCESS);
+	if (NULL == hServiceMgr)
+	{
+		printf("OpenSCManagerW 失败, %d\n", GetLastError());
+		return FALSE;
+	}
+	//printf("打开服务控制管理器成功.\n");
+	// 创建驱动服务
+	SC_HANDLE hServiceDDK = NULL; // NT驱动程序服务句柄
+	hServiceDDK = CreateServiceW(
+		hServiceMgr,
+		lpszDriverName,
+		lpszDriverName,
+		SERVICE_ALL_ACCESS,
+		SERVICE_KERNEL_DRIVER,
+		SERVICE_DEMAND_START,
+		SERVICE_ERROR_IGNORE,
+		szDriverFullPath,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		NULL);
+	if (NULL == hServiceDDK)
+	{
+		DWORD dwErr = GetLastError();
+		if (dwErr != ERROR_IO_PENDING && dwErr != ERROR_SERVICE_EXISTS)
+		{
+			printf("创建驱动服务失败, %d\n", dwErr);
+			return FALSE;
+		}
+	}
+	//printf("创建驱动服务成功.\n");
+	// 驱动服务已经创建，打开服务
+	hServiceDDK = OpenServiceW(hServiceMgr,lpszDriverName,SERVICE_ALL_ACCESS);
+	if (!StartService(hServiceDDK, NULL, NULL))
+	{
+		DWORD dwErr = GetLastError();
+		if (dwErr != ERROR_SERVICE_ALREADY_RUNNING)
+		{
+			printf("运行驱动服务失败, %d\n", dwErr);
+			return FALSE;
+		}
+	}
+	//printf("运行驱动服务成功.\n");
+	if (hServiceDDK)
+	{
+		CloseServiceHandle(hServiceDDK);
+	}
+	if (hServiceMgr)
+	{
+		CloseServiceHandle(hServiceMgr);
+	}
+	return TRUE;
+}
+
+// 卸载驱动
+void UnLoadDriver(PCWSTR lpszDriverName)
+{
+	SC_HANDLE hServiceMgr = OpenSCManagerW(0,0,SC_MANAGER_ALL_ACCESS);
+	SC_HANDLE hServiceDDK = OpenServiceW(hServiceMgr,lpszDriverName,SERVICE_ALL_ACCESS);
+	SERVICE_STATUS SvrStatus;
+	ControlService(hServiceDDK,SERVICE_CONTROL_STOP,&SvrStatus);
+	DeleteService(hServiceDDK);
+	if (hServiceDDK)
+	{
+		CloseServiceHandle(hServiceDDK);
+	}
+	if (hServiceMgr)
+	{
+		CloseServiceHandle(hServiceMgr);
+	}
+}
+
+// 获取PDE
+DWORD *GetPDE(DWORD addr)
+{
+	return (DWORD *)(0xc0600000 + ((addr >> 18) & 0x3ff8));
+}
+
+// 获取PTE
+DWORD *GetPTE(DWORD addr)
+{
+	return (DWORD *)(0xc0000000 + ((addr >> 9) & 0x7ffff8));
+}
+
+// 构建调用门（提权、有参）
+USHORT CreateCallGate(DWORD pBaseAddress, DWORD nParam)
+{
+	HANDLE hDevice = CreateFileW(DRIVER_LINK,GENERIC_READ|GENERIC_WRITE,0,0,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,0);
+	if (hDevice == INVALID_HANDLE_VALUE)
+	{
+		return 0;
+	}
+	USHORT CallGateDescriptor; // 调用门选择子
+	DWORD dwRetBytes; // 返回的字节数
+	DWORD InBuffer[2];
+	InBuffer[0] = pBaseAddress;
+	InBuffer[1] = nParam;
+	DeviceIoControl(hDevice,OPER_CALL_GATE_R0,InBuffer,8,&CallGateDescriptor,sizeof(USHORT),&dwRetBytes,NULL);
+	if (dwRetBytes != 2 || CallGateDescriptor == 0)
+	{
+		printf("构造调用门失败.\n");
+		return 0;
+	}
+	CloseHandle(hDevice);
+	return CallGateDescriptor;
+}
+
+// 以0环权限调用某个裸函数，支持传参
+BOOL CallInRing0(PVOID pFuncion, PDWORD pParam, DWORD nParam)
+{
+	// 命令驱动构建调用门
+	USHORT CallGateDescriptor = CreateCallGate((DWORD)pFuncion,nParam);
+	if (CallGateDescriptor == 0)
+	{
+		return FALSE;
+	}
+	// 构造调用门描述符
+	USHORT buff[3] = {0};
+	buff[2] = CallGateDescriptor;
+	// 参数压栈
+	if (nParam && pParam)
+	{
+		for (DWORD i = 0; i < nParam; i++)
+		{
+			__asm
+			{
+				mov eax,pParam;
+				push [eax];
+			}
+			pParam++;
+		}
+	}	
+	// 调用门调用
+	__asm call fword ptr [buff]; // 长调用，使用调用门提权
+	return TRUE;
+}
+
+// API函数过写拷贝，其实就是将函数线性地址的PDE，PTE改成可写
+// 参数0：要过写拷贝的函数地址
+// 参数1：PDE线性地址
+// 参数2：PTE线性地址
+void __declspec(naked) BypassApiWriteCopyNaked()
+{
+	__asm
+	{
+		pushad;
+		pushfd;
+	}
+	__asm
+	{		
+		// R/W = 1, U/S = 1
+		mov eax,[esp+0x24+0x8+0x0]; // 参数2，PTE的地址
+		or dword ptr [eax],0x00000006;
+		mov eax,[esp+0x24+0x8+0x4]; // 参数1，PDE的地址
+		or dword ptr [eax],0x00000006;
+		mov eax,[esp+0x24+0x8+0x8]; // 参数0，要过写拷贝的函数地址
+		invlpg [eax]; // 清除TLB缓存
+	}
+	__asm
+	{
+		popfd;
+		popad;
+		retf 0xC;
+	}
+}
+
+// 过写拷贝
+void BypassApiWriteCopy()
+{
+	// MessageBoxA 挂物理页，不这样操作，MessageBoxA的PTE可能是无效的
+	__asm
+	{
+		mov eax, dword ptr ds:[MessageBoxA];
+		mov eax,[eax];
+	}
+	// MessageBoxA过写拷贝	
+	DWORD pParam[3];
+	pParam[0] = (DWORD)MessageBoxA;
+	pParam[1] = (DWORD)GetPDE(pParam[0]);
+	pParam[2] = (DWORD)GetPTE(pParam[0]);
+	CallInRing0(BypassApiWriteCopyNaked, pParam,3);
+}
+
+// HOOK MessageBoxA
+// 理论上可以 HOOK User32.dll 里的任意函数
+BOOL HookUser32Api()
+{
+	// 遍历USER32.DLL函数
+	// 构造一个中断门，返回中断号（如0x20）,通过该中断门可跳转监视函数
+	// 只要函数开头是 0x8BFF 的函数，都改成中断门调用（例如中断号是0x20，就将头两字节改成 0xCD20）
+	// 监视函数：在3环申请一块共享内存，将shellcode拷贝进去，设置为可执行。
+	// 监视函数的功能是记录函数调用并打印参数
+
+	HANDLE hDevice = CreateFileW(DRIVER_LINK,GENERIC_READ|GENERIC_WRITE,0,0,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,0);
+	if (hDevice == INVALID_HANDLE_VALUE)
+	{
+		printf("打开设备失败.\n");
+		return FALSE;
+	}
+	USHORT IntGateNum; // 中断号
+	DWORD dwRetBytes; // 返回的字节数
+
+	DeviceIoControl(hDevice,OPER_HOOK,NULL,0,&IntGateNum,sizeof(USHORT),&dwRetBytes,NULL);
+	if (dwRetBytes != 2 || IntGateNum == 0)
+	{
+		printf("构造中断门失败.\n");
+		return FALSE;
+	}
+	CloseHandle(hDevice);
+
+	// HOOK MessageBoxA
+	USHORT IntInstructions = (IntGateNum << 8);
+	IntInstructions |= (USHORT)0x00CD;	
+	*(PUSHORT)MessageBoxA = IntInstructions;
+
+	return TRUE;
+}
+
+// 从驱动获取调用记录
+void UpdateApiCallRecord()
+{
+	HANDLE hDevice = CreateFileW(DRIVER_LINK,GENERIC_READ|GENERIC_WRITE,0,0,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,0);
+	if (hDevice == INVALID_HANDLE_VALUE)
+	{
+		printf("打开设备失败.\n");
+		return;
+	}
+	APICALLRECORD ApiCallRecord;
+	DWORD dwRetBytes; // 返回的字节数
+	while (!GetAsyncKeyState('Q'))
+	{
+		Sleep(200);
+		DeviceIoControl(hDevice,OPER_GET_APICALLRECORD,NULL,0,&ApiCallRecord,sizeof(ApiCallRecord),&dwRetBytes,NULL);
+		if (dwRetBytes == 0) 
+		{
+			//printf("无API调用记录.\n");
+			continue;
+		}
+		if (ApiCallRecord.pApiAddress == (DWORD)MessageBoxA)
+		{
+			printf("MessageBoxA(%x, %x, %x, %x);\n", \
+				ApiCallRecord.Param[0],ApiCallRecord.Param[1],ApiCallRecord.Param[2],ApiCallRecord.Param[3]);
+		}		
+	}
+
+
+	CloseHandle(hDevice);
+}
+
+
